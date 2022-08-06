032a95c43f84b8b6f7d32533201da4a0b3a48573
gabypega1997 <gabypega1997@gmail.com>
2022-08-06 17:50:23 +0200

    add logout views

--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
 
 def login_view(request):
     if request.method == "POST":
@@ -26,4 +27,7 @@ def login_view(request):
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "user/login.html", {
+        "message": "Logged out."
+    })
